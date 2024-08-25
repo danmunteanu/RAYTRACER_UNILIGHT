9308af45bdbea1e6e34044a471a3fe542a5637d8
Dan Munteanu <epsdel@gmail.com>
2024-08-25 20:18:22 +0300

    Further tweaks.

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -28,19 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
+            panelRender = new Panel();
+            progressRender = new ProgressBar();
+            button1 = new Button();
             SuspendLayout();
             // 
-            // Form1
+            // panelRender
+            // 
+            panelRender.Location = new Point(12, 12);
+            panelRender.Name = "panelRender";
+            panelRender.Size = new Size(675, 383);
+            panelRender.TabIndex = 0;
+            // 
+            // progressRender
+            // 
+            progressRender.Location = new Point(12, 401);
+            progressRender.Name = "progressRender";
+            progressRender.Size = new Size(564, 23);
+            progressRender.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(582, 401);
+            button1.Name = "button1";
+            button1.Size = new Size(105, 23);
+            button1.TabIndex = 2;
+            button1.Text = "RENDER";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Name = "Form1";
+            ClientSize = new Size(703, 443);
+            Controls.Add(button1);
+            Controls.Add(progressRender);
+            Controls.Add(panelRender);
+            Name = "MainForm";
             Text = "Form1";
             Load += MainForm_Load;
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panelRender;
+        private ProgressBar progressRender;
+        private Button button1;
     }
 }
--- MainForm.cs
+++ MainForm.cs
@@ -10,11 +10,11 @@ namespace UnilightRaytracer
         {
             InitializeComponent();
 
- /*           Scene scene = new Scene();
+            Scene scene = new Scene();
             //  Storage storage = new SerializationStorage();
             Controller control = new Controller();
-            ObservableImage image = new ObservableImage(800, 600, java.awt.image.BufferedImage.TYPE_INT_RGB);
-            RenderFrame mainFrame = null;
+            ObservableImage image = new ObservableImage(800, 600, 0/*java.awt.image.BufferedImage.TYPE_INT_RGB*/);
+//            RenderFrame mainFrame = null;
             Raytracer rt = new Raytracer();
             RaytracerThread rtThread = new RaytracerThread(rt);
 
@@ -26,22 +26,22 @@ namespace UnilightRaytracer
             cam.setViewportHeight(9);
 
             //  setup main frame
-            javax.swing.JFrame.setDefaultLookAndFeelDecorated(true);
+  /*          javax.swing.JFrame.setDefaultLookAndFeelDecorated(true);
             mainFrame = new RenderFrame(image);
             utils.Common.centerFrame(mainFrame);
             mainFrame.setController(control);
-            mainFrame.setVisible(true);
+            mainFrame.setVisible(true);*/
 
             //  setup raytracer
             rt.setScene(scene);
             rt.setCamera(cam);
             rt.setImage(image);
             rt.setMaxTraceDepth(5);
-            rt.setCallback(mainFrame.createOrGetProgressCallback());
+            //rt.setCallback(mainFrame.createOrGetProgressCallback());
 
             //  setup raytracer thread
-            rtThread.setPriority(5);
-            rtThread.start();  // start rt thread
+            /*rtThread.setPriority(5);
+            rtThread.start();  // start rt thread*/
 
             //  setup controller
             control.setRaytracer(rt);
@@ -49,7 +49,7 @@ namespace UnilightRaytracer
             control.setScene(scene);
 
             //  add mainFrame as an observer for the image
-            image.getSubject().addObserver(mainFrame);*/
+            /*image.getSubject().addObserver(mainFrame);*/
         }
 
         private void MainForm_Load(object sender, EventArgs e)
